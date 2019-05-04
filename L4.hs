fe2f89f0d8fa9b807dcdffca15bd8e540c9f76c9
Alex <48485023+Liliya2000@users.noreply.github.com>
2019-05-04 22:50:39 +0300

    Add files via upload

---
+++ L4.hs
@@ -0,0 +1,12 @@
+﻿import Data.Char
+import Data.List
+main = do
+	a <- getLine
+	xs <- getLine
+	file <- readFile xs
+	if a == "l" then do
+		print (map toLower file)
+	else 
+		if a == "u" then do
+			print (map toUpper file)
+		else putStrLn ("не тот символ")
